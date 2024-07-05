a86cc8bb67f050f5aae3d463c1bb443c4be1a578
Talles L <talleslasmar@gmail.com>
2024-07-05 15:54:37 -0300

    Linting configuration module

--- _configuration.py
+++ _configuration.py
@@ -15,7 +15,7 @@ documents: Any = lambda: None
 documents.directory = path.expanduser('~/Downloads')
 documents.extension = '.pdf'
 
-indexed_hashes_filepath = './indexed_hashes.txt'
+indexed_hashes_filepath = './indexed_hashes.txt'  # pylint: disable=invalid-name
 
 ollama: Any = lambda: None
 ollama.base_url = 'http://localhost:11434'
@@ -23,9 +23,11 @@ ollama.model = 'llama3'
 
 prompt: Any = lambda: None
 prompt.input_variables = ['question', 'context']
-prompt.template = 'You are a helpful assistant knowledgeable about the contents of the PDFs.'
-'Here is some context from the PDFs:'
-'{context}'
-''
-'Q: {question}'
-'A:'
+prompt.template = """
+    You are a helpful assistant knowledgeable about the contents of the PDFs.
+    Here is some context from the PDFs:
+    {context}
+
+    Q: {question}
+    A:
+"""
