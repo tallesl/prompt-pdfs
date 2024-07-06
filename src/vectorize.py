bed780a759cafc4f9f35a71eeef4bfba95693bb6
Talles L <talleslasmar@gmail.com>
2024-07-06 19:50:23 -0300

    Reorganized vectorize script functions

--- src/vectorize.py
+++ src/vectorize.py
@@ -8,7 +8,6 @@ extension.
 # pylint: disable=redefined-outer-name
 
 from functools import reduce
-from os import listdir, path
 
 from _internals.vector_store import initialize_chroma, verify_chroma, index_embedding
 from _internals.hash_store import list_indexed_hashes, index_hash, calculate_file_hash
@@ -16,50 +15,11 @@ from _internals.utilities import log, set_signals, list_files_with_extension
 import configuration
 
 
-def list_non_indexed_files(indexed_hashes: set[str], documents_configuration) -> set[str]:
+def vectorize() -> None:
     """
-    Lists the non-indexed files in the configured directory with the configured extension.
+    Indexes on a ChromaDB database the embeddings of the files residing in the configured directory
+    and extension.
     """
-
-    log(
-        f'Listing non-indexed {documents_configuration.extension} '
-        f'from: {documents_configuration.directory}'
-    )
-
-    selected_filepaths = list_files_with_extension(
-        documents_configuration.directory, documents_configuration.extension
-    )
-
-    selected_files_hashes = {
-        f: calculate_file_hash(f)
-        for f in selected_filepaths
-    }
-
-    non_indexed_hashes = {
-        f: h
-        for f, h in selected_files_hashes.items()
-        if h not in indexed_hashes
-    }
-
-    non_indexed_files = non_indexed_hashes.keys()
-
-    sorted_files = sorted(non_indexed_files)
-
-    concatenated_filepaths = reduce(
-        lambda f, concatenated:
-            f'{f}\n\t{concatenated}' if concatenated else f,
-        sorted_files,
-        ''
-    )
-
-    printable_filepaths = concatenated_filepaths if concatenated_filepaths else " (none)"
-
-    log(f'New files to be indexed:{printable_filepaths}')
-
-    return set(sorted_files)
-
-
-if __name__ == '__main__':
     # set process signals
     set_signals()
 
@@ -68,10 +28,65 @@ if __name__ == '__main__':
     verify_chroma(chroma, configuration.chroma)
 
     # listing new files to be indexed
-    indexed_hashes = list_indexed_hashes(configuration.indexed_hashes_filepath)
-    non_indexed_files = list_non_indexed_files(indexed_hashes, configuration.documents)
+    extension: str = configuration.documents.extension
+    log(
+        f'Listing non-indexed {extension} '
+        f'from: {configuration.documents.directory}'
+    )
 
-    # indexing files
-    for f in non_indexed_files:
-        index_hash(configuration.indexed_hashes_filepath, f)
+    indexed_hashes = list_indexed_hashes(configuration.indexed_hashes_filepath)
+    source_filepaths = list_files_with_extension(
+        configuration.documents.directory, extension
+    )
+    non_indexed_filepaths = list_non_indexed_files(indexed_hashes, source_filepaths)
+    printable_filepaths = get_printable_file_list(non_indexed_filepaths)
+
+    log(f'New files to be indexed:{printable_filepaths}')
+
+    # indexing both embeddings and hash for each file
+    for f in non_indexed_filepaths:
         index_embedding(chroma, f)
+        index_hash(configuration.indexed_hashes_filepath, f)
+
+
+def list_non_indexed_files(indexed_hashes: set[str], source_filepaths: list[str]) -> set[str]:
+    """
+    Lists from the given source files the ones that are not in the given indexed hashes.
+    """
+
+    source_files_hashes = {
+        f: calculate_file_hash(f)
+        for f in source_filepaths
+    }
+
+    non_indexed_hashes = {
+        f: h
+        for f, h in source_files_hashes.items()
+        if h not in indexed_hashes
+    }
+
+    non_indexed_files = non_indexed_hashes.keys()
+
+    return set(non_indexed_files)
+
+
+def get_printable_file_list(filepaths: set[str]) -> str:
+    """
+    Returns a printable list of the given filepaths.
+    """
+    sorted_files = sorted(filepaths)
+
+    concatenated_filepaths = reduce(
+        lambda f, concatenated:
+        f'{f}\n\t{concatenated}' if concatenated else f,
+        sorted_files,
+        ''
+    )
+
+    printable_filepaths = concatenated_filepaths if concatenated_filepaths else " (none)"
+
+    return printable_filepaths
+
+
+if __name__ == '__main__':
+    vectorize()
